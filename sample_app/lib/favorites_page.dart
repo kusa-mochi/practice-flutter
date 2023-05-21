c58b3683822a74a30193902e41e8ff12156e67d2
mochi <whoatemyapplepie@gmail.com>
2023-05-21 21:24:22 +0900

    convert FavoritesPage Widget to StatelessWidget.

--- sample_app/lib/favorites_page.dart
+++ sample_app/lib/favorites_page.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sample_app/my_app_state.dart';
 
-class FavoritesPage extends StatefulWidget {
-  @override
-  State<FavoritesPage> createState() => _FavoritesPageState();
-}
-
-class _FavoritesPageState extends State<FavoritesPage> {
+class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
